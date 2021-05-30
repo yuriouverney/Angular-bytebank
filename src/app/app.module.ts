fe7c6461a3cf81d5ca8ebd7a99e3fc7ae6c13b73
Yuri Ouverney <yuri.ouverney@hotmail.com>
2021-05-29 22:42:47 -0300

    primeiro módulo

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.componente';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/nova-transferencia/nova-transferencia.componente.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//o  "app" como prefixo no selector é devido ao prefixo que está colocado no angular.json
+@Component({
+    selector:'app-nova-transferencia',
+    templateUrl:'./nova-transferencia.component.html',
+    styleUrls:['./nova-transferencia.component.scss']
+})
+
+
+export class NovaTransferenciaComponent{
+
+}
